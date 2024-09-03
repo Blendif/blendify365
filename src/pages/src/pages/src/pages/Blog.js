99dc4982a9d913925897f0483a2391a73b87d3c4
Blendif <blendify365@gmail.com>
2024-09-03 06:45:10 +0100

    Create Blog.js

---
+++ src/pages/src/pages/src/pages/Blog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BlogArticle from '../components/BlogArticle';
+
+const Blog = () => {
+  return (
+    <div>
+      <h1>Blog</h1>
+      <BlogArticle title="How to Boost Productivity" author="A.Sterling" content="Productivity tips and tricks..." />
+    </div>
+  );
+};
+
+export default Blog;
